129c6fb2598fef274520cd2675d9b34c559ae593
Mountqi <mountqi@163.com>
2016-05-06 16:37:28 +0800

    sub-corp customers template fix

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 from config import config
 
